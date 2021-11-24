378a7f08c484a475bb20f4dc78c35267a49b0806
Ceci <carbel.cecilia@gmail.com>
2021-11-24 13:54:22 +0100

    inicial proyect

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template >
+  <div class="fondo">
+    <Navbar />
+    <Tarea  />
+  </div>
+  
+  
+  
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+import Tarea from './components/Tarea';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Tarea
+  }
+}
+</script>
+
+<style>
+  .fondo {
+    background-image: url('./assets/fondo_task.jpg');
+    height: 100vh;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">App Lista Tareas</span>
+            </div>
+        </nav>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
